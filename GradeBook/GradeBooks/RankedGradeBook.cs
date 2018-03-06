615c53dbdea596597356b2bd33fb6f98e95cc260
mrmantle <matthew.mantle@tribalgroup.com>
2018-03-06 20:37:01 +0000

    RankedGrades-A

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -15,6 +16,22 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 throw new InvalidOperationException();
+            }
+
+            //find how many students is twenty percent of the total number of students
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+
+            //order the list of grades for all students
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            //create condition that takes input grade and determines what rank grade it will be
+            if (grades[threshold-1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
             }
 
             return 'F';
